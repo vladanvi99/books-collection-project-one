6b5b6ddb55621aff1d23c8c084bf35202c456b2b
SimonGrchevski <simongrcevski@gmail.com>
2021-07-05 14:28:26 +0200

    Add the remove function

--- js/main.js
+++ js/main.js
@@ -1,48 +1,75 @@
-const addBtn = document.querySelector('.add');
-const bookTitle = document.getElementById('title');
-const bookAuthor = document.getElementById('author');
-const bookListWrap = document.querySelector('.book-ul');
+document.addEventListener("DOMContentLoaded", function() {
+  const addBtn = document.querySelector('.add');
+  const bookTitle = document.getElementById('title');
+  const bookAuthor = document.getElementById('author');
+  const bookListWrap = document.querySelector('.book-ul');
 
 
-let bookCollection = {
-    books:[]
-};
-// GET BOOK COLLECTION FROM LOCALSTORAGE
-function getBooksFromLocalStorage() {
-    if(JSON.parse(localStorage.getItem('bookCollection'))) {
-        bookCollection = JSON.parse(localStorage.getItem('bookCollection'));
-        console.log(bookCollection)
+  let bookCollection = {
+    books: []
+  };
+  // GET BOOK COLLECTION FROM LOCALSTORAGE
+  function getBooksFromLocalStorage() {
+    if (JSON.parse(localStorage.getItem('bookCollection'))) {
+      bookCollection = JSON.parse(localStorage.getItem('bookCollection'));
+      console.log(bookCollection)
     }
-}
-getBooksFromLocalStorage();
-// IMPLEMENT BOOKS
-function implementBooks() {
-    bookCollection.books.forEach((book,index) => {
-        bookListWrap.innerHTML += `<li>
-        <div class="book-info">
-          <p><span class="book-title">"${book.title}"</span> by <span class="book-author">${book.author}</span></p>
-        </div>
-        <button type="button" data-key=${index}>Remove</button>
-      </li>`
+  }
+  getBooksFromLocalStorage();
+  // IMPLEMENT BOOKS
+  function implementBooks() {
+
+    bookListWrap.innerHTML = '';
+    bookCollection.books.forEach((book, index) => {
+      bookListWrap.innerHTML += `<li>
+          <div class="book-info">
+            <p><span class="book-title">"${book.title}"</span> by <span class="book-author">${book.author}</span></p>
+          </div>
+          <button type="button" class="removeBtn" data-key=${index}>Remove</button>
+        </li>`
     })
-}
-implementBooks();
+  }
+  if (document.querySelector('.book-ul')) {
+    implementBooks();
+  }
 
-// ADD BOOK
-function addBook(e) {
+  let removeBtn = [...document.querySelectorAll('.removeBtn')];
+
+  // ADD BOOK
+  function addBook(e) {
     if (bookTitle.value.length <= 2 || bookAuthor.value.length <= 2) {
-        e.preventDefault();
+      e.preventDefault();
     } else {
-        bookCollection.books.push({ title: bookTitle.value, author: bookAuthor.value });
-        updateLocalStorage();
+      bookCollection.books.push({ title: bookTitle.value, author: bookAuthor.value });
+      updateLocalStorage();
     }
-}
+  }
 
-function updateLocalStorage() {
-  localStorage.setItem('bookCollection', JSON.stringify(bookCollection));
+  //Remove Book
+  const removeBook = (btn) => {
 
-}
+    bookCollection.books = bookCollection.books.filter((book, i) => i !== Number(btn.dataset.key));
 
-if (document.querySelector('.add')) {
+    updateLocalStorage();
+    getBooksFromLocalStorage();
+    implementBooks();
+    removeBtn = [...document.querySelectorAll('.removeBtn')];
+    removeBtn.forEach((button) => button.addEventListener('click', () => removeBook(button)));
+
+
+  }
+
+  function updateLocalStorage() {
+    localStorage.setItem('bookCollection', JSON.stringify(bookCollection));
+
+  }
+
+  if (document.querySelector('.add')) {
     addBtn.addEventListener('click', (e) => addBook(e));
-}
+  }
+
+
+
+  removeBtn.forEach((button) => button.addEventListener('click', () => removeBook(button)));
+
+});
